8adcab17f4c6095030c5cb4b296b2ddf66fbc504
farsana nkm <fasrsanankm24@gmail.comgit config --global user.email fasrsanagit config --global user.email fasrsanankm24@gmail.com>
2024-02-19 11:39:11 +0530

    hi

---
+++ functionjs.js
@@ -0,0 +1,107 @@
+
+// function Addition(a,b){
+// return a+b;
+// }
+// let c= Addition (2,4)
+// console.log(c);
+
+
+
+
+
+// function sub(a,b) {
+//     return (a-b);
+// }
+// let c= sub(5,2);
+// console.log(c);
+
+
+
+
+// function mul(a,b){
+//     return (a*b);
+// }
+// let c= mul(4,2);
+// console.log(c);
+
+
+
+
+
+// function div(a,b){
+//     return (a/b);
+// }
+// let c= div(4,2);
+// console.log(c);
+
+
+
+
+// Addition = (a,b)=>{
+//     return a+b;
+// } 
+// let c= Addition(2,4);
+// console.log(c);
+
+
+
+
+//  sub = (a,b)=>{
+//     return a-b;
+//  }
+//  let c = sub(3,1);
+//  console.log(c);
+
+
+
+
+// mult=(a,b)=>{
+//     return a*b;
+// }
+// let c = mult(3,4);
+// console.log(c);
+
+
+
+
+// div =(a,b)=>{
+//     return a/b;
+// }
+// let c = div(4,2);
+// console.log(c);
+
+
+
+
+
+
+// let a=parseInt(prompt("enter value of a"));
+// let b=parseInt(prompt("enter value of b"));
+// function Addition(a,b){
+//     return(a+b);
+// }
+// let c=Addition(a,b);
+// console.log(c);
+
+
+
+
+// const a=[1,2,3,4,5];
+// let b=a.findIndex(myfunction);
+// function myfunction(value,index,array) {
+//     return value > 3
+    
+// }
+// console.log(b);
+
+
+
+
+// const a=[1,2,3,4,5];
+// let b=a.find(myfunction);
+// function myfunction(value,index,array) {
+//     return value > 3
+    
+// }
+// console.log(b);
+
---
+++ sample.js
@@ -0,0 +1,69 @@
+// document.getElementById('demo').innerHTML=10
+// document.write("hello world")
+// window.alert("jsndunsjn")
+
+// let a=10;
+// let b=20  ;
+// console.log(a+b);
+
+
+/*const a =10;
+{
+    const a=20;
+}
+
+console.log(a);*/
+/*var a =10;
+var a = 30;
+console.log(a);*/
+
+/*let a =10;
+a=20;
+console.log(a);*/
+
+/*const a=10;
+const b=20;
+console.log(a+b);
+
+let and const lann block work aka var ll block work akola
+block working=eg:*/
+
+/*var a=10;
+{
+    var a=30;
+}
+console.log(a);*/ 
+
+// let a=10;
+// {
+//     b=30;
+// }
+// console.log(a);
+
+/*const a=10;{
+    const b=30;
+}
+console.log(a);*/
+
+/*let a=10;
+a=20;
+console.log(a);*/
+
+
+
+// function Addition(a,b){
+//     return a+b;
+   
+// }
+
+// let c= Addition(1,2)
+// console.log(c);
+
+
+// Adittion = (a,b) =>{
+//     return a+b
+// }
+
+// let c = Adittion(1,2)
+// console.log(c);
+
